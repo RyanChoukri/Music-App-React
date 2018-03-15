19dc0425ea66e26eda6a8ddb4b2f352681f2df78
ryan <ryanchourkri@gmail.com>
2018-03-15 14:41:40 +0100

    Update todolist

--- website/src/pages/TodolistPage.js
+++ website/src/pages/TodolistPage.js
@@ -4,26 +4,8 @@ import Counter from '../_components/Counter/index'
 
 class TodolistPage extends Component {
     render() {
-        const items = [
-            {
-                name : "Devenir riche",
-                state : false
-            },
-            {
-                name : "devenir dev",
-                state : false
-            },
-            {
-                name : "autre",
-                state : false
-            },
-            {
-                name : "rien",
-                state : false
-            }
-        ];
         return (
-            <Todolist items={items}/>
+            <Todolist />
             // <Counter/>
         )
     }
